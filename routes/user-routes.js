3f6de27a333d483bd205a26215c183e54010fda4
gabriel-digital <62797405+gabriel-digital@users.noreply.github.com>
2020-05-07 15:42:38 +0200

    modified response on successfull user login

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -81,7 +81,7 @@ router.post("/user/log_in", async (req, res) => {
       return res.status(400).json({ error: { message: "invalid password" } });
     }
     // everything's fine, welcome user !
-    return res.json({ user.account.username, user.account.token, message: `Welcome ${user.account.username} !` });
+    return res.json({ username: user.account.username, token: user.token, message: `Welcome ${user.account.username} !` });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
